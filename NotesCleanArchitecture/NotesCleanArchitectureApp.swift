23a6c027ac6c06cc9a08753a9cf4c6a72010d849
Rangga Biner <ranggabiner@gmail.com>
2024-09-28 23:18:15 +0700

    Initial Commit

---
+++ NotesCleanArchitecture/NotesCleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesCleanArchitectureApp.swift
+//  NotesCleanArchitecture
+//
+//  Created by Rangga Biner on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesCleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
